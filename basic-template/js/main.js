22bc9ee550850ba870c9aadb6918d9d60e644284
TreeBeard2112 <jansensd@mail.uc.edu>
2022-11-06 22:40:13 -0500

    Traffic,score as fraction,timer skips card

--- basic-template/js/main.js
+++ basic-template/js/main.js
@@ -6,11 +6,13 @@ window.onload=function() {
 
     //Info 
     var score = 0;
+    var total = 1;
     var index = 0;
 
     //Traffic Data
     var answers = [3,3,3,3,3,1,2,2,1,1,3,3,3,2,1,1,1,3,3,3,2,1,1,1,3,3,3,3,3,1,1,1,1,1,3,3,3,3,1,1,1,3,1,1];
     var levels = [1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,1.10,2.1,2.2,2.3,2.4,2.5,2.6,2.7,3.1,3.2,3.3,3.4,3.5,3.6,3.7,4.1,4.2,4.3,4.4,4.5,4.6,4.7,4.8,4.9,4.10,5.1,5.2,5.3,5.4,5.5,5.6,5.7,5.8,5.9,5.10];
+
 
     //Time
     var time = 20
@@ -19,24 +21,36 @@ window.onload=function() {
     if (time >= 0) {
         document.getElementById("time").innerText = time;
     } else  {
-        document.getElementById("time").innerText = "Game Over;-;";
+        scoreCard.innerHTML = score + "/" + total;
+        total++;
+
+        extension.src = "TrafficExtensionPics/" + levels[index] + ".PNG"
+        traffic.src = "TrafficPics/" + levels[index] + ".PNG"
+        index++;
+
+        clearInterval(x);
+        time = 20;
+        x = setInterval(watchMaker,1000);
     }
     }
     var x = setInterval(watchMaker, 1000)
+
+
     
 
     // Button Functionality
-    var image = document.getElementById("traffic");
+    var extension = document.getElementById("traffic-extension");
+    var traffic = document.getElementById("traffic");
     var acceptBtn = document.getElementById("accept");
     acceptBtn.onclick = function() {
         if(answers[index] == 1) {
             score ++;
-        } else {
-            score --;
         }
-        scoreCard.innerHTML = score;
+        scoreCard.innerHTML = score + "/" + total;
+        total++
 
-        image.src = "TrafficExtensionPics/" + levels[index] + ".PNG"
+        extension.src = "TrafficExtensionPics/" + levels[index] + ".PNG"
+        traffic.src = "TrafficPics/" + levels[index] + ".PNG"
         index++;
 
         clearInterval(x);
@@ -47,12 +61,12 @@ window.onload=function() {
     rejectBtn.onclick = function() {
         if(answers[index] == 3) {
             score ++;
-        } else {
-            score --;
         }
-        scoreCard.innerHTML = score;
+        scoreCard.innerHTML = score + "/" + total;
+        total++;
 
-        image.src = "TrafficExtensionPics/" + levels[index] + ".PNG"
+        extension.src = "TrafficExtensionPics/" + levels[index] + ".PNG"
+        traffic.src = "TrafficPics/" + levels[index] + ".PNG"
         index++;
 
         clearInterval(x);
